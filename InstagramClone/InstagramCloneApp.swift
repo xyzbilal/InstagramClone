cf13c514d7f717f4768ff24f5207dbf7f7ae39f5
bilal simsek <xyzbilal@gmail.com>
2023-07-10 13:09:28 +0300

    Initial Commit

---
+++ InstagramClone/InstagramCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramCloneApp.swift
+//  InstagramClone
+//
+//  Created by Bilal SIMSEK on 10.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct InstagramCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
