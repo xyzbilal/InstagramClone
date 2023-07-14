fd2be9d43c189a67789f6ea5c4f207fd61b0f905
bilal simsek <xyzbilal@gmail.com>
2023-07-14 23:01:59 +0300

    Feedview immplemented

---
+++ InstagramClone/Core/Feed/FeedView.swift
@@ -0,0 +1,41 @@
+//
+//  FeedVşew.swift
+//  InstagramClone
+//
+//  Created by Bilal SIMSEK on 10.07.2023.
+//
+
+import SwiftUI
+
+struct FeedView: View {
+    var body: some View {
+      NavigationStack {
+            ScrollView{
+                LazyVStack(spacing:20) {
+                    ForEach(Post.MOCK_POSTS) { post in
+                        FeedCell(post:post)
+                    }
+                }
+                .padding(.top,8)
+            }.navigationTitle("Feed")
+              .navigationBarTitleDisplayMode(.inline)
+              .toolbar {
+                  ToolbarItem(placement: .navigationBarLeading) {
+                      Image("instagram")
+                          .resizable()
+                          .frame(width: 100,height: 32)
+                  }
+                  ToolbarItem(placement: .navigationBarTrailing) {
+                      Image(systemName: "paperplane")
+                          .imageScale(.large)
+                  }
+              }
+        }
+    }
+}
+
+struct FeedView_Previews: PreviewProvider {
+    static var previews: some View {
+        FeedView()
+    }
+}
